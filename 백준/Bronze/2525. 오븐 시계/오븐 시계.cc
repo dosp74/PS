aa79a4e23f56a6d098686948bb9ba9e161b4237b
HAN JONGSEO <pantene74@kw.ac.kr>
2025-04-02 00:09:37 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    
+    A += C / 60;
+    B += C % 60;
+    
+    if (B >= 60) {
+        A++;
+        B %= 60;
+    }
+    
+    A %= 24;
+    
+    cout << A << " " << B;
+    
+    return 0;
+}
