8ac057f2248567cab089a8221da07ae87de41655
HAN JONGSEO <pantene74@kw.ac.kr>
2025-04-02 12:40:58 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    
+    if (A == B && B == C)
+        cout << 10000 + A * 1000;
+    else if (A == B || A == C)
+        cout << 1000 + A * 100;
+    else if (B == C)
+        cout << 1000 + B * 100;
+    else
+        cout << max(A, max(B, C)) * 100;
+    
+    return 0;
+}
