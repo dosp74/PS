263a008f7a500a9b884ad79e44e5f3c6ef3d0bdf
HAN JONGSEO <pantene74@kw.ac.kr>
2025-04-03 13:58:58 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int T, A, B;
+    cin >> T;
+    
+    while(T--) {
+        cin >> A >> B;
+        cout << A + B << "\n";
+    }
+    
+    return 0;
+}
