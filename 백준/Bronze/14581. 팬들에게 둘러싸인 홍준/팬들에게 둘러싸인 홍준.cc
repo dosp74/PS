85166a7aa439c219281f2ea55d38a7814faedd50
HAN JONGSEO <pantene74@kw.ac.kr>
2025-05-01 17:35:37 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string id;
+    getline(cin, id);
+    
+    cout << ":fan::fan::fan:\n:fan::";
+    cout << id;
+    cout << "::fan:\n:fan::fan::fan:";
+    
+    return 0;
+}
