a267ce8311f3497fcc745416082b2b479b7f14bd
HAN JONGSEO <pantene74@kw.ac.kr>
2025-05-05 17:08:38 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    switch (N) {
+        case 0:
+            cout << "YONSEI";
+            break;
+        case 1:
+            cout << "Leading the Way to the Future";
+            break;
+        default:
+            break;
+    }
+    
+    return 0;
+}
