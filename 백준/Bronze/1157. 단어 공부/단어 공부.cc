47742eb1a16aa4c497e46e2db3fb0f218a6c5958
HAN JONGSEO <pantene74@kw.ac.kr>
2025-05-15 22:49:01 +0900

    [Bronze I] Title: 단어 공부, Time: 40 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.cc
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -4,38 +4,38 @@
 using namespace std;
 
 int main() {
-	string str;
-	cin >> str;
-	
-	int alphabet[26] = { 0 };
-	
-	for (int i = 0; i < str.size(); i++) {
-		char c = str[i];
-		
-		if (islower(c))
-			c = toupper(c);
-		
-		alphabet[c - 65]++;
-	}
-	
-	int maxCount = 0;
-	int maxIndex;
-	bool isMany = false;
-	
-	for (int i = 0; i < 26; i++) {
-		if (alphabet[i] > maxCount) {
-			maxCount = alphabet[i];
-			maxIndex = i;
+    string str;
+    cin >> str;
+    
+    int alphabet[26] = { 0 };
+    
+    for (int i = 0; i < str.size(); i++) {
+        char c = str[i];
+        
+        if (islower(c))
+            c = toupper(c);
+        
+        alphabet[c - 65]++;
+    }
+    
+    int maxCount = 0;
+    int maxIndex;
+    bool isMany = false;
+    
+    for (int i = 0; i < 26; i++) {
+        if (alphabet[i] > maxCount) {
+            maxCount = alphabet[i];
+            maxIndex = i;
             isMany = false;
-		}
-		else if (alphabet[i] == maxCount)
-		isMany = true;
-	}
-	
-	if (isMany)
-		cout << "?";
-	else
-		cout << char(maxIndex + 65);
-	
-	return 0;
+        }
+        else if (alphabet[i] == maxCount)
+            isMany = true;
+    }
+    
+    if (isMany)
+        cout << "?";
+    else
+        cout << char(maxIndex + 65);
+    
+    return 0;
 }
