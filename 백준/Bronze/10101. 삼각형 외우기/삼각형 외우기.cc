96136544e17c5720f25272fe6191e5e83c8f1170
HAN JONGSEO <pantene74@kw.ac.kr>
2025-05-25 20:02:46 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if (a + b + c != 180) {
+        cout << "Error";
+        
+        return 0;
+    }
+    
+    if (a == 60 && b == 60 && c == 60)
+        cout << "Equilateral";
+    else if (a + b + c == 180 && (a == b || a == c || b == c))
+        cout << "Isosceles";
+    else
+        cout << "Scalene";
+    
+    return 0;
+}
