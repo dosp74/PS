8cae6d96d887dfdd33f3b60c699f1d645ca2301a
HAN JONGSEO <pantene74@kw.ac.kr>
2025-05-26 09:47:37 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string words[5];
+    for (int i = 0; i < 5; i++)
+        cin >> words[i];
+    
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i < words[j].length())
+                cout << words[j][i];
+        }
+    }
+    
+    return 0;
+}
