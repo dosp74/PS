24aa7b2a5b20fca41cf198a524fa8baba6f2ab64
HAN JONGSEO <pantene74@kw.ac.kr>
2025-06-14 17:25:45 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;
+    
+    int count = 0;
+    
+    for (int i = 0; i < word.size(); i++) {
+        if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
+            count++;
+    }
+    
+    cout << count;
+    
+    return 0;
+}
