aa96c27cc4a5a514b533418790c33c4d26b00af2
HAN JONGSEO <pantene74@kw.ac.kr>
2025-06-28 19:04:29 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> arr(5);
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+    
+    cout << (arr[0] + arr[1] + arr[2] + arr[3] + arr[4]) / 5 << "\n";
+    
+    sort(arr.begin(), arr.end());
+    
+    cout << arr[2];
+    
+    return 0;
+}
