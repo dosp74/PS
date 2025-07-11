ed214bfccc2b4c9ea932b4ac725c5a4764a56718
HAN JONGSEO <pantene74@kw.ac.kr>
2025-07-11 20:00:16 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 568 ms, Memory: 209752 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    
+    unordered_set<string> us;
+    
+    for (int i = 0; i < S.size(); i++) {
+        for (int j = i; j < S.size(); j++) {
+            string partS = S.substr(i, j - i + 1);
+            us.insert(partS);
+        }
+    }
+    
+    cout << us.size();
+    
+    return 0;
+}
