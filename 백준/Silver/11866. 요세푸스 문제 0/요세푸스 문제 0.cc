28db165b810bd14747eacf306fdb1caf484e8d2e
HAN JONGSEO <pantene74@kw.ac.kr>
2025-07-16 23:16:12 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    
+    int N, K;
+    cin >> N >> K;
+    
+    queue<int> josephus;
+    for (int i = 1; i <= N; i++) {
+        josephus.push(i);
+    }
+    
+    cout << "<";
+    
+    while (josephus.size() != 1) {
+        for (int i = 1; i < K; i++) {
+            josephus.push(josephus.front());
+            josephus.pop();
+        }
+        cout << josephus.front() << ", ";
+        josephus.pop();
+    }
+    
+    cout << josephus.front() << ">";
+    
+    return 0;
+}
