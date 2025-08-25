41067d240c1c681d832e259352e02b5c97ce4fd6
HAN JONGSEO <pantene74@kw.ac.kr>
2025-08-26 00:53:34 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N;
+    cin >> N;
+    
+    vector<vector<int>> cost(N, vector<int>(3));
+    for (int i = 0; i < N; i++) {
+        cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
+    }
+    
+    // dp[i][c]는 i번째 집을 c색으로 칠했을 때의 최소 비용
+    // c = 0: R, 1: G, 2: B
+    vector<vector<int>> dp(N, vector<int>(3));
+    
+    dp[0][0] = cost[0][0];
+    dp[0][1] = cost[0][1];
+    dp[0][2] = cost[0][2];
+    
+    for (int i = 1; i < N; i++) {
+        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+    }
+    
+    cout << min(dp[N - 1][0], min(dp[N - 1][1], dp[N - 1][2]));
+    
+    return 0;
+}
