b625dcaf9ec36e32266f6003eeb2112c4adcad91
HAN JONGSEO <pantene74@kw.ac.kr>
2025-09-06 23:08:42 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 80 ms, Memory: 11316 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 100001;
+vector<int> graph[MAX]; // 인접 리스트
+int visited[MAX];
+int order[MAX];
+int cnt = 0;
+
+void dfs(int vertex) {
+    visited[vertex] = 1; // 현재 정점 방문 표시
+    order[vertex] = ++cnt; // 현재 정점이 몇 번째로 방문되었는지 저장
+    
+    for (int next : graph[vertex]) {
+        if (!visited[next]) {
+            dfs(next);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N, M, R;
+    cin >> N >> M >> R;
+    
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    
+    for (int i = 1; i <= N; i++) {
+        sort(graph[i].begin(), graph[i].end());
+    }
+    
+    dfs(R);
+    
+    for (int i = 1; i <= N; i++) {
+        cout << order[i] << "\n";
+    }
+    
+    return 0;
+}
