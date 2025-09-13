1039db9477ab45175b9b1db2cfaad3853aa535b6
HAN JONGSEO <pantene74@kw.ac.kr>
2025-09-13 14:56:10 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 12496 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,18 +1,15 @@
-#include <iostream>
-#include <queue>
-#include <tuple>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int MAX = 1001;
 int tomato[MAX][MAX];
-bool visited[MAX][MAX];
 int dx[4] = {0, 0, -1, 1}; // 좌우상하
 int dy[4] = {-1, 1, 0, 0};
 int M, N; // M은 상자의 가로 칸의 수, N은 상자의 세로 칸의 수
-int cnt = 0;
 
 int bfs() {
     queue<tuple<int, int, int>> q;
+    int days = 0;
     
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
@@ -23,32 +20,18 @@ int bfs() {
     }
     
     while (!q.empty()) {
-        int x, y, cc;
-        tie(x, y, cc) = q.front();
+        auto [x, y, d] = q.front();
         q.pop();
-        visited[x][y] = true;
+        days = max(days, d);
         
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             
-            if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny]) {
+            if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
                 if (tomato[nx][ny] == 0) {
                     tomato[nx][ny] = 1;
-                    visited[nx][ny] = true;
-                    q.push({nx, ny, cc + 1});
-                    
-                    if (cnt < cc + 1) {
-                        cnt = cc + 1;
-                    }
-                    
-                    continue;
-                }
-                
-                if (tomato[nx][ny] == -1) {
-                    if (cc > cnt) {
-                        cnt = cc;
-                    }
+                    q.push({nx, ny, d + 1});
                 }
             }
         }
@@ -56,18 +39,18 @@ int bfs() {
     
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            if (tomato[i][j] == 0 && !visited[i][j]) {
+            if (tomato[i][j] == 0) {
                 return -1;
             }
         }
     }
     
-    return cnt;
+    return days;
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr);
     
     cin >> M >> N;
     
