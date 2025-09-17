f3b4c464c498ba985b70aad5a851730d6bb0eacc
HAN JONGSEO <pantene74@kw.ac.kr>
2025-09-17 14:45:26 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N, K;
+    cin >> N >> K;
+    
+    for (int i = 0; i < N; i++) {
+        int A, B;
+        cin >> A >> B;
+    }
+    
+    cout << "비와이";
+    
+    return 0;
+}
