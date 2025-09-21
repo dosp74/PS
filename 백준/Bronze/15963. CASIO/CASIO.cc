a2610a7fb0e93174a52cb46c1138e5ad599f48e6
HAN JONGSEO <pantene74@kw.ac.kr>
2025-09-21 17:36:42 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    long long N, M;
+    cin >> N >> M;
+    
+    if (N == M) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+    
+    return 0;
+}
