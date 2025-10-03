56d55e8ac4b7fda2841a5604437dea91c9696a56
HAN JONGSEO <pantene74@kw.ac.kr>
2025-10-03 18:25:00 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int T;
+    cin >> T;
+    
+    vector<int> dp(11); // dp[i] = i를 1, 2, 3의 합으로 나타내는 방법의 수
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    dp[4] = 7;
+    
+    for (int i = 5; i <= 10; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+    
+    while (T--) {
+        int n;
+        cin >> n;
+        
+        cout << dp[n] << "\n";
+    }
+    
+    return 0;
+}
