f4c597aa37f28f938e05da15ef98387b1dc52b2a
HAN JONGSEO <pantene74@kw.ac.kr>
2025-11-20 21:06:08 +0900

    [Bronze III] Title: 369, Time: 0 ms, Memory: 2020 KB, Score: 5 point -BaekjoonHub

---
+++ 백준/Bronze/17614. 369/369.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    
+    int cnt = 0;
+    
+    for (int i = 1; i <= N; i++) {
+        string s = to_string(i);
+        
+        for (int j = 0; j < s.length(); j++) {
+            if (s[j] == '3' || s[j] == '6' || s[j] == '9') {
+                cnt++;
+                
+                break;
+            }
+        }
+    }
+    
+    cout << cnt;
+    
+    return 0;
+}
