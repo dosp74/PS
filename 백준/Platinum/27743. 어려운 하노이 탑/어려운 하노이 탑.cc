33a3c89e5f7551cb403ef88edb74424ce49a14c5
HAN JONGSEO <pantene74@kw.ac.kr>
2025-12-15 01:29:22 +0900

    [Platinum III] Title: 어려운 하노이 탑, Time: 0 ms, Memory: 2020 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Platinum/27743. 어려운 하노이 탑/어려운 하노이 탑.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long MOD = 1000000007;
+
+long long pow_mod(long long base, long long exp) {
+    long long result = 1;
+    
+    while (exp) {
+        if (exp & 1) {
+            result = result * base % MOD;
+        }
+        
+        base = base * base % MOD;
+        exp >>= 1;
+    }
+    
+    return result;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    long long N, M;
+    cin >> N >> M;
+    
+    long long answer;
+    
+    if (M == 1) {
+        answer = pow_mod(2, N) - 1;
+    }
+    else {
+        long long temp = pow_mod(2, N + 1);
+        answer = (M * (temp - 2) - 1) % MOD;
+    }
+    
+    cout << answer;
+    
+    return 0;
+}
