6ec6407068cad435c5d82c3d01ec491dd84b2e89
HAN JONGSEO <pantene74@kw.ac.kr>
2025-12-26 14:55:06 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int col[9];
+
+bool promising(int i) {
+    for (int j = 1; j < i; j++) {
+        if (col[j] > col[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+void sequences(int i) {
+    if (promising(i)) {
+        if (i == M) {
+            for (int j = 1; j <= M; j++) {
+                cout << col[j] << " ";
+            }
+            
+            cout << "\n";
+        }
+        else {
+            for (int k = 1; k <= N; k++) {
+                col[i + 1] = k;
+                sequences(i + 1);
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> N >> M;
+    
+    sequences(0);
+    
+    return 0;
+}
