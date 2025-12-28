946f66cdfd2eed53dcc14ae8387043fa5d2508bc
HAN JONGSEO <pantene74@kw.ac.kr>
2025-12-28 17:54:47 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+vector<int> sequence;
+int col[8];
+
+void sequences(int i) {
+    if (i == M) {
+        for (int j = 0; j < i; j++) {
+            cout << col[j] << " ";
+        }
+        
+        cout << "\n";
+    }
+    else {
+        for (int k = 0; k < N; k++) {
+            if (i > 0 && sequence[k] < col[i - 1]) {
+                continue;
+            }
+            else {
+                col[i] = sequence[k];
+                sequences(i + 1);
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> N >> M;
+    sequence.resize(N);
+    for (int i = 0; i < N; i++) {
+        cin >> sequence[i];
+    }
+    
+    sort(sequence.begin(), sequence.end());
+    sequences(0);
+    
+    return 0;
+}
