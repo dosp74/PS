3a4be6fd1a70a9f4648a8a0d6e43fc139f2027b5
HAN JONGSEO <pantene74@kw.ac.kr>
2026-02-21 16:28:05 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    
+    int graph[101][101];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> graph[i][j];
+        }
+    }
+    
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (graph[i][k] && graph[k][j]) {
+                    graph[i][j] = 1;
+                }
+            }
+        }
+    }
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << graph[i][j] << " ";
+        }
+        
+        cout << "\n";
+    }
+    
+    return 0;
+}
