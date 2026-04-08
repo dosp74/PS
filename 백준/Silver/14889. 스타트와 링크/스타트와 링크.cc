fdc33d05bf63eae66af6e0cb95cd346dcd9a7e21
JongSeo Han <pantene74@kw.ac.kr>
2026-04-09 02:39:45 +0900

    [Silver I] Title: 스타트와 링크, Time: 72 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -2,36 +2,35 @@
 using namespace std;
 
 int N;
-vector<vector<int>> stats;
+vector<vector<int>> board;
 int minValue = INT_MAX;
 bool picked[20];
 
-void solve(int i, int pickedCount) {
+void dfs(int i, int pickedCount) {
     if (pickedCount == N / 2) {
-        int currentValue1 = 0;
-        int currentValue2 = 0;
+        int curValue1 = 0;
+        int curValue2 = 0;
         
         for (int j = 0; j < N; j++) {
             for (int k = j + 1; k < N; k++) {
                 if (picked[j] && picked[k]) {
-                    currentValue1 += stats[j][k] + stats[k][j];
+                    curValue1 += board[j][k] + board[k][j];
                 }
                 
                 if (!picked[j] && !picked[k]) {
-                    currentValue2 += stats[j][k] + stats[k][j];
+                    curValue2 += board[j][k] + board[k][j];
                 }
             }
         }
         
-        int currentResult = abs(currentValue1 - currentValue2);
-        minValue = min(minValue, currentResult);
+        minValue = min(minValue, abs(curValue1 - curValue2));
         
         return;
     }
     else {
         for (int j = i; j < N; j++) {
             picked[j] = true;
-            solve(j + 1, pickedCount + 1);
+            dfs(j + 1, pickedCount + 1);
             picked[j] = false;
         }
     }
@@ -42,15 +41,16 @@ int main() {
     cin.tie(nullptr);
     
     cin >> N;
-    stats.resize(N, vector<int>(N));
+    board.resize(N, vector<int>(N));
+    
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            cin >> stats[i][j];
+            cin >> board[i][j];
         }
     }
     
     picked[0] = true;
-    solve(1, 1);
+    dfs(1, 1);
     
     cout << minValue;
     
