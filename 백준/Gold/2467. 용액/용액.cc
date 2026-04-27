3c1a92d818348c1b95315f6999204815a3b0ebd3
JongSeo Han <pantene74@naver.com>
2026-04-28 00:14:58 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    
+    vector<long long> arr(N);
+    
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    
+    int left = 0;
+    int right = N - 1;
+    
+    long long best = LLONG_MAX;
+    long long answerA = arr[left];
+    long long answerB = arr[right];
+    
+    while (left < right) {
+        long long sum = arr[left] + arr[right];
+        
+        if (abs(sum) < best) {
+            best = abs(sum);
+            answerA = arr[left];
+            answerB = arr[right];
+        }
+        
+        if (sum < 0) {
+            left++;
+        }
+        else if (sum > 0) {
+            right--;
+        }
+        else {
+            break;
+        }
+    }
+    
+    cout << answerA << " " << answerB;
+    
+    return 0;
+}
