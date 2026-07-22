d7a0c08b81e821f56717c7adb169f2463f9f98b2
JongSeo Han <pantene74@naver.com>
2026-07-22 19:22:32 +0900

    [level 0] Title: 홀짝 구분하기, Time: 1.46 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    
+    if (n % 2 == 0) {
+        cout << n << " is even";
+    }
+    else {
+        cout << n << " is odd";
+    }
+    
+    return 0;
+}
