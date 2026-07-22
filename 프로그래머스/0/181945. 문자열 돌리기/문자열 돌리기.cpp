0a279b122bbfb8a1886d5ff6a45a2bdd7b70ae62
JongSeo Han <pantene74@naver.com>
2026-07-22 19:20:26 +0900

    [level 0] Title: 문자열 돌리기, Time: 1.18 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string str;
+    cin >> str;
+    
+    for (int i = 0; i < str.length(); i++) {
+        cout << str[i] << "\n";
+    }
+    
+    return 0;
+}
