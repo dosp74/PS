af9c5edc65e4216117678c822160eb8679662e0f
JongSeo Han <pantene74@naver.com>
2026-07-22 19:18:42 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 1.15 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string str1, str2;
+    cin >> str1 >> str2;
+    
+    cout << str1 + str2;
+    
+    return 0;
+}
