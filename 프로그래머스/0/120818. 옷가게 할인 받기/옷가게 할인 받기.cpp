bc29054b04727406f3c5d02204af3d145941df41
JongSeo Han <pantene74@naver.com>
2026-09-14 01:33:50 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 4.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = price;
+    
+    if (price >= 500000) {
+        answer = price * 8 / 10;
+    }
+    else if (price >= 300000) {
+        answer = price * 9 / 10;
+    }
+    else if (price >= 100000) {
+        answer = price * 95 / 100;
+    }
+    
+    return answer;
+}
