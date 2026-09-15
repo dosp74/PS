99dad2dd0af8b7ba54df1753fe9af534fb12af46
JongSeo Han <pantene74@naver.com>
2026-09-15 16:26:56 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.00 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    
+    for (char c : my_string) {
+        for (int i = 0; i < n; i++) {
+            answer += c;
+        }
+    }
+    
+    return answer;
+}
